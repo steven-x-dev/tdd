658b5964fb3fd28e75f18364b27b99a36b59d0ae
steven-x-dev <steven-dev@qq.com>
2020-09-14 18:47:52 +0800

    [steven] feat: implemented "should_not_insert_mommy_given_vowel_percentage_lte_30_percent"

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -7,32 +7,42 @@ public class Mommifier {
 
     public String mommify(String str) {
 
+        if (calculateVowelPercentage(str) <= 0.3) return str;
+
         StringBuilder sb = new StringBuilder();
 
         boolean isPrevVowel = false;
 
         for (char c : str.toCharArray()) {
-
             if (isVowel(c)) {
-
                 sb.append(c);
                 isPrevVowel = true;
-
             } else {
-
-                if (isPrevVowel)
+                if (isPrevVowel) {
                     sb.append(MOMMY);
-
+                }
                 sb.append(c);
-
                 isPrevVowel = false;
             }
         }
 
-        if (isVowel(str.charAt(str.length() - 1)))
+        if (isVowel(str.charAt(str.length() - 1))) {
             sb.append(MOMMY);
+        }
 
         return sb.toString();
+    }
+
+
+    private static double calculateVowelPercentage(String str) {
+
+        int vowelCount = 0;
+
+        for (char c : str.toCharArray()) {
+            if (isVowel(c)) vowelCount++;
+        }
+
+        return (double) vowelCount / str.length();
     }
 
 
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -33,10 +33,16 @@ class MommifierTest {
             "ddauui" + INSERT;
 
     private static final String NO_VOWEL_EXP = "qwrtyp";
-    private static final String NO_VOWEL_ACT = "qwrtyp";
+    private static final String NO_VOWEL_ACT = NO_VOWEL_EXP;
 
     private static final String ALL_VOWELS_EXP = "aeiou";
-    private static final String ALL_VOWELS_ACT = "aeiou" + INSERT;
+    private static final String ALL_VOWELS_ACT = ALL_VOWELS_EXP + INSERT;
+
+    private static final String LT_30_EXP = "qwrtypacip";
+    private static final String LT_30_ACT = LT_30_EXP;
+
+    private static final String EQ_30_EXP = "ypacwpuibp";
+    private static final String EQ_30_ACT = EQ_30_EXP;
 
 
     @Test
@@ -61,6 +67,24 @@ class MommifierTest {
 
         expected.add(mommifier.mommify(ALL_VOWELS_EXP));
         actual.add(ALL_VOWELS_ACT);
+
+        assertIterableEquals(expected, actual);
+    }
+
+
+    @Test
+    void should_not_insert_mommy_given_vowel_percentage_lte_30_percent() {
+
+        Mommifier mommifier = new Mommifier();
+
+        List<String> expected = new ArrayList<>();
+        List<String> actual = new ArrayList<>();
+
+        expected.add(mommifier.mommify(LT_30_EXP));
+        actual.add(LT_30_ACT);
+
+        expected.add(mommifier.mommify(EQ_30_EXP));
+        actual.add(EQ_30_ACT);
 
         assertIterableEquals(expected, actual);
     }
