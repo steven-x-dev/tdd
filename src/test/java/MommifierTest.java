e72515e1985211e1ddbf91255c6a6cd4770e3956
steven-x-dev <steven-dev@qq.com>
2020-09-14 18:17:29 +0800

    [steven] refactor: refactored MommifierTest, removed comments and made code readable by extracting test output and expected values into constants with proper names

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -7,6 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MommifierTest {
 
+    private static final String INSERT = "mommy";
+
+    private static final String START_WITH_CONSONANT_LT_30_PERC_EXP = "daofpyimseddauuid";
+    private static final String START_WITH_CONSONANT_LT_30_PERC_ACT =
+            "dao"    + INSERT +
+            "fpyi"   + INSERT +
+            "mse"    + INSERT +
+            "ddauui" + INSERT +
+            "d";
+
+    private static final String END_WITH_CONSONANT_LT_30_PERC_EXP = "aofpyimseddauuid";
+    private static final String END_WITH_CONSONANT_LT_30_PERC_ACT =
+            "ao"     + INSERT +
+            "fpyi"   + INSERT +
+            "mse"    + INSERT +
+            "ddauui" + INSERT +
+            "d";
+
+    private static final String END_WITH_VOWEL_LT_30_PERC_EXP = "aofpyimseddauui";
+    private static final String END_WITH_VOWEL_LT_30_PERC_ACT =
+            "ao"     + INSERT +
+            "fpyi"   + INSERT +
+            "mse"    + INSERT +
+            "ddauui" + INSERT;
+
+    private static final String NO_VOWEL_EXP = "qwrtyp";
+    private static final String NO_VOWEL_ACT = "qwrtyp";
+
+    private static final String ALL_VOWELS_EXP = "aeiou";
+    private static final String ALL_VOWELS_ACT = "aeiou" + INSERT;
+
 
     @Test
     void should_insert_mommy_afterwards_given_continuous_vowels() {
@@ -16,59 +47,20 @@ class MommifierTest {
         List<String> expected = new ArrayList<>();
         List<String> actual = new ArrayList<>();
 
+        expected.add(mommifier.mommify(START_WITH_CONSONANT_LT_30_PERC_EXP));
+        actual.add(START_WITH_CONSONANT_LT_30_PERC_ACT);
 
-        /* -------------- start test cases -------------- */
+        expected.add(mommifier.mommify(END_WITH_CONSONANT_LT_30_PERC_EXP));
+        actual.add(END_WITH_CONSONANT_LT_30_PERC_ACT);
 
-        // 1. start with consonant
-        expected.add(mommifier.mommify("daofpyimseddauuid"));
+        expected.add(mommifier.mommify(END_WITH_VOWEL_LT_30_PERC_EXP));
+        actual.add(END_WITH_VOWEL_LT_30_PERC_ACT);
 
-        actual.add(
-                "dao" + "mommy" +
-                "fpyi" + "mommy" +
-                "mse" + "mommy" +
-                "ddauui" + "mommy" +
-                "d" +
-                "");
+        expected.add(mommifier.mommify(NO_VOWEL_EXP));
+        actual.add(NO_VOWEL_ACT);
 
-
-        // 2. start with vowel
-        expected.add(mommifier.mommify("aofpyimseddauuid"));
-
-        actual.add(
-                "ao" + "mommy" +
-                "fpyi" + "mommy" +
-                "mse" + "mommy" +
-                "ddauui" + "mommy" +
-                "d" +
-                "");
-
-
-        // 3. end with vowel
-        expected.add(mommifier.mommify("aofpyimseddauui"));
-
-        actual.add(
-                "ao" + "mommy" +
-                "fpyi" + "mommy" +
-                "mse" + "mommy" +
-                "ddauui" + "mommy" +
-                "");
-
-
-        // 4. no vowel
-        expected.add(mommifier.mommify("qwrtyp"));
-
-        actual.add("qwrtyp");
-
-
-        // 5. all vowels
-        expected.add(mommifier.mommify("aeiou"));
-
-        actual.add(
-                "aeiou" + "mommy" +
-                "");
-
-        /* -------------- end test cases -------------- */
-
+        expected.add(mommifier.mommify(ALL_VOWELS_EXP));
+        actual.add(ALL_VOWELS_ACT);
 
         assertIterableEquals(expected, actual);
     }
